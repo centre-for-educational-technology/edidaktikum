799c99b4ced59d3060bdb1f61f5abea0f4a34665
RKoitsalu <raimond@172.17.2.219>
2013-10-01 11:41:17 +0300

    fix #59

--- edidaktikum.js
+++ edidaktikum.js
@@ -3,12 +3,12 @@
 
   Drupal.behaviors.ed_task = {
     attach: function(context, settings){
-      $('#edit-ed-field-category', context).find('label[for="edit-ed-field-category-und"]').on('click', function(){
+      $('#edit-ed-field-category', context).find('label[for="edit-ed-field-category-und"]').once('my-helper', function(){$('#edit-ed-field-category', context).find('label[for="edit-ed-field-category-und"]').on('click', function(){
         $('#edit-ed-field-category-und').slideToggle("slow");
-      })
-      $('#edit-ed-field-competence', context).find('label[for="edit-ed-field-competence-und"]').on('click', function(){
+      })})
+      $('#edit-ed-field-competence', context).find('label[for="edit-ed-field-competence-und"]').once('my-helper', function(){$('#edit-ed-field-competence', context).find('label[for="edit-ed-field-competence-und"]').on('click', function(){
         $('#edit-ed-field-competence-und').slideToggle("slow");
-      })
+      })})
     }
   }
 
