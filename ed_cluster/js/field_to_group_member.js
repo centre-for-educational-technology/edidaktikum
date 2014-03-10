24ad45f25a366654ddd07e3490aaddea791908cd
Pjotr Savitski <pjotr.savitski@gmail.com>
2014-03-10 17:06:56 +0200

    References #167, added javascript and ajax endpoint.
    Both should later on be used with correct field. The logic will depend
    on a field name, the javascript will modify how group selection behaves
    by changing users select field (bringing in the data according to groups
    being selected).

---
+++ ed_cluster/js/field_to_group_member.js
@@ -0,0 +1,24 @@
+(function($) {
+    Drupal.behaviors.ed_cluster = {
+        'attach': function(context) {
+            $('select[name="og_group_ref[und][0][default][]"]', context).on('change', function() {
+                var _groups = $(this);
+                var _users = $('select[name="field_to_group_member[und][]"]');
+                $.post( Drupal.settings.basePath + Drupal.settings.pathPrefix + 'clusters/get/groups_members', { groups: $(this).val() }, function(data) {
+                    var currently_selected = _users.val();
+                    _users.empty();
+                    if (data) {
+                        $.each(data, function(key, single) {
+                            if ($.inArray(single.value, currently_selected) != -1) {
+                                _users.append('<option value="'+single.value+'" selected="selected">'+single.name+'</option>');
+                            } else {
+                                _users.append('<option value="'+single.value+'">'+single.name+'</option>');
+                            }
+                        });
+                    }
+                });
+            });
+        }
+    }
+})(jQuery);
+
