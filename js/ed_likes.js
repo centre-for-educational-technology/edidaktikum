93eb17c1203cf3bd5c2db3a265b2a71fd9660f9e
Pjotr Savitski <pjotr.savitski@gmail.com>
2014-05-13 17:11:17 +0300

    Fixes #223, all done. Requires running update and clearning caches.
    The only own content type missing this functionality is a folder, but it
    does not have panel nor should it be viewed much.

---
+++ js/ed_likes.js
@@ -0,0 +1,21 @@
+(function ($) {
+    Drupal.behaviors.ed_likes = {
+        attach: function(context, settings) {
+                    $('.ed-node-statistics', context).find('.ed-node-statistics-likes a').on('click', function(e) {
+                        e.preventDefault();
+
+                        $.ajax({
+                            url: Drupal.settings.basePath + Drupal.settings.pathPrefix + 'node/' + $(this).data('id') + '/edidaktikum/action/like',
+                            dataType: 'json',
+                            type: 'POST',
+                            success: function(response) {
+                                if (true == response.success) {
+                                    $(e.currentTarget).parent().find('span.ed-likes-count').html(Drupal.t('@count likes', { '@count': response.count }));
+                                }
+                            }
+                        });
+                    });
+                }
+    };
+})(jQuery);
+
