ffddb5656c091d88c71cd12c9d7581db36e6c75f
Pjotr Savitski <gnum@tlu.ee>
2015-03-05 18:11:46 +0200

    Issue #330, implemented initial collection, not what is should be.
    The initial implementation is mostly a rip off from portfolio. With
    fields and tunes for the collection. Still needs a lot of work until
    being complete.

---
+++ ed_collection/ed_collection_page.tpl.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @file
+ * Collection view.
+ */
+
+?>
+
+<!-- Navbar -->
+<div id="navbar" class="navbar navbar-medium navbar-inverse navbar-static-top">
+	<div class="navbar-inner">
+		<div class="container">
+			<a class="brand" href="<?php print $front_page ?>">
+                <?php print $site_name ?>
+            </a>
+		</div>
+	</div>
+</div>
+
+<?php print drupal_render($node_view); ?>
+
