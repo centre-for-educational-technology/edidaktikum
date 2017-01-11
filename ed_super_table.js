c0f861e0448cf31deec5bcee5cf981d7750f86ed
hazarada <karliraudsepp@hazarada.htk.tlu.ee>
2017-01-11 16:41:28 +0200

    Close #479
    Complete rewrite. Also contains irrelevant bits of WIP super table.

--- ed_super_table.js
+++ ed_super_table.js
@@ -68,7 +68,42 @@ var data4 = {
 	};
 }
 
+{	//	super_drag_handler
+
+	function super_drag_handler(el, callback, button) {
+		this.button_mask = (button == "left" ? 1 : (button == "right" ? 2 : 4));
+		this.callback = callback;
+
+		el.addEventListener("mousedown", function(e) {
+			if ((e.buttons & this.button_mask) != this.button_mask) return;
+			this.last_clientX = e.clientX;
+			this.last_clientY = e.clientY;
+			document.addEventListener("mousemove", this.move_handler);
+		}.bind(this));
+
+		this.move_handler = function(e) {
+			if ((e.buttons & this.button_mask) != this.button_mask) {
+				document.removeEventListener("mousemove", this.move_handler);
+				return;
+			}
+			var dx = e.clientX - this.last_clientX;
+			var dy = e.clientY - this.last_clientY;
+			this.last_clientX = e.clientX;
+			this.last_clientY = e.clientY;
+
+			this.callback(dx, dy);
+		}.bind(this);
+	}
+
+}
+
+
 {	//	super_table
+
+	//	options
+	//	lock horizontal / vertical cell spacers
+	//	display horitzontal / vertical headers
+	//	lock / unlock statistical rows
 
 	super_table.prototype.create_table = function(width, height) {
 		if (typeof this.table !== 'undefined') this.el.removeChild(this.table);
@@ -91,14 +126,114 @@ var data4 = {
 		}
 	};
 
+	super_table.prototype.resize_on_move = function(e) {
+		var dx = e.clientX - this.last_clientX;
+		var dy = e.clientY - this.last_clientY;
+
+	};
+
+	Object.defineProperty(super_table.prototype, 'width', {
+		'get' : function() { return parseInt(this.el.style.width); },
+		'set' : function(val) {	this.el.style.width = val + "px"; }
+	});
+
+	Object.defineProperty(super_table.prototype, 'height', {
+		'get' : function() { return parseInt(this.el.style.height); },
+		'set' : function(val) {	this.el.style.height = val + "px"; }
+	});
+
+	Object.defineProperty(super_table.prototype, 'stat_width', {
+		'get' : function() { return parseInt(this.config_box.style.width); },
+		'set' : function(val) {	
+			this.stat_bottom.style.right = val + "px";
+			this.stat_right.style.width = val + "px";
+			this.config_box.style.width = val + "px";
+		}
+	});
+
+	Object.defineProperty(super_table.prototype, 'stat_height', {
+		'get' : function() { return parseInt(this.config_box.style.height); },
+		'set' : function(val) {	
+			this.stat_bottom.style.height = val + "px";
+			this.stat_right.style.bottom = val + "px";
+			this.config_box.style.height = val + "px";
+		}
+	});
+
 	function super_table(el) {
 		this.data = new super_table_data(data4);
-		console.log(el);
 		this.el = (typeof el !== 'undefined') ? el : document.createElement("div");
 		this.el.className = "super_table";
-		console.log(this.el);
 
-		this.create_table(this.data.width, this.data.height);
+		//	clone auto width
+		this.width = this.el.getBoundingClientRect().width;
+		this.height = 300;
+	
+		{	//	widget resize handles, level 0
+			/*
+			this.bottom_sizer = document.createElement("div");
+			this.bottom_sizer.className = "bottom_sizer";
+			this.el.appendChild(this.bottom_sizer);
+			new super_drag_handler(this.bottom_sizer, function(x, y) {
+				this.height += y;
+			}.bind(this), "left");
+
+			this.right_sizer = document.createElement("div");
+			this.right_sizer.className = "right_sizer";
+			this.el.appendChild(this.right_sizer);
+			new super_drag_handler(this.right_sizer, function(x, y) {
+				this.width += x;
+			}.bind(this), "left");
+			*/
+
+
+			this.bottom_right_sizer = document.createElement("div");
+			this.bottom_right_sizer.className = "bottom_right_sizer";
+			this.el.appendChild(this.bottom_right_sizer);
+			new super_drag_handler(this.bottom_right_sizer, function(x, y) {
+				this.height += y;
+				this.width += x;
+			}.bind(this), "left");
+
+			this.scroll_horizontal = document.createElement("div");
+			this.scroll_horizontal.className = "scroll_horizontal";
+			this.el.appendChild(this.scroll_horizontal);
+		}
+
+		{	//	stat and config partitions, level 1
+			this.content = document.createElement("div");
+			this.content.className = "content";
+			this.el.appendChild(this.content);
+
+			this.stat_right = document.createElement("div");
+			this.stat_right.className = "stat_right";
+			this.content.appendChild(this.stat_right);
+
+			this.stat_bottom = document.createElement("div");
+			this.stat_bottom.className = "stat_bottom";
+			this.content.appendChild(this.stat_bottom);
+
+			this.config_box = document.createElement("div");
+			this.config_box.className = "config_box";
+			this.content.appendChild(this.config_box);
+
+			this.stat_width = 120;
+			this.stat_height = 40;
+		}
+		/*
+		this.top_bar = document.createElement("div");
+		this.top_bar.className = "top_bar";
+		this.el.appendChild(this.top_bar);
+
+		this.bottom_bar = document.createElement("div");
+		this.bottom_bar.className = "bottom_bar";
+		this.el.appendChild(this.bottom_bar);
+
+		this.scroll_box = document.createElement("div");
+		this.scroll_box.className = "scroll_box";
+		this.el.appendChild(this.scroll_box);
+*/
+		//this.create_table(this.data.width, this.data.height);
 		
 	}
 
