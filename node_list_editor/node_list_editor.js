3bbb246ba5b13b2ee788fbc19957eecd722dc875
Karli Raudsepp <karliraudsepp@Karlis-MacBook-Pro.local>
2018-01-12 12:28:25 +0200

    IE 9, 10, 11 compatibility

--- node_list_editor/node_list_editor.js
+++ node_list_editor/node_list_editor.js
@@ -11,43 +11,8 @@
 		return el;
 	}
 
-	class taxonomy_item {
-		constructor(node, parent, callback_changed) {
-			this.node = node;
-			this.parent = parent;
-			this.child = undefined;
-			this.el = build("div", "taxonomy_item");
-			this.callback_changed = callback_changed || function(value) {};
-			this._value = undefined;
-
-			if (parent === undefined) {
-				this.el_del = build("div", "edf_mini_button", this.el, "❌");
-				this.el_del.addEventListener("click", function() {
-					this.callback_changed(undefined, this.value);
-					this.el.parentElement.removeChild(this.el);
-				}.bind(this));
-
-				this.el.setAttribute("data-root", "");
-				this.root = this;
-			} else {
-				parent.el.appendChild(this.el);
-				this.root = parent.root;
-			}
-
-			this.el_sel = build("select", undefined, this.el);
-			build("option", undefined, this.el_sel, "-");
-			for (var i = 0; i < node.children.length; i++) {
-				var opt = build("option", undefined, this.el_sel, node.children[i].name);
-				opt.value = node.children[i].tid;
-			}
-
-			this.el_sel.addEventListener("change", function() { this.select(); }.bind(this));
-
-			this.paths = {};
-			this.map_node_path(node, []);
-		}
-
-		map_node_path(node, path) {
+	function taxonomy_item(node, parent, callback_changed) {
+		this.map_node_path = function(node, path) {
 			this.paths[node.tid] = path.slice();
 			this.paths[node.tid].push(node.tid);
 			path.push(node.tid);
@@ -55,16 +20,16 @@
 			for (var i = 0; i < node.children.length; i++) {
 				this.map_node_path(node.children[i], path.slice());
 			}
-		}
+		}.bind(this);
 
-		get_node_by_tid(tid) {
+		this.get_node_by_tid = function(tid) {
 			for (var i = 0; i < this.node.children.length; i++) {
 				if (this.node.children[i].tid == tid) return this.node.children[i];
 			}
 			return undefined;
-		}
+		}.bind(this);
 
-		select(node) {
+		this.select = function(node) {
 			var sel_node = node;
 			if (sel_node === undefined) {
 				for (var i = 0; i < this.node.children.length; i++) {
@@ -91,130 +56,109 @@
 
 			if (sel_node === undefined || sel_node.children.length == 0) return;	
 			this.child = new taxonomy_item(sel_node, this, this.callback_changed);		
-		}
+		}.bind(this);
 
 
-		get value() {
-			return this.root._value;
-		}
-
-		set value(v) {
-			this.root._value = v;
-			if (!(v in this.paths)) return;
-			var path = this.paths[v];
-			var ti = this;
-
-			for (var i = 1; i < path.length; i++) {
-				ti.select(ti.get_node_by_tid(path[i]));
-				ti = ti.child;
-			}
-		}
-
-		remove_current_child() {
+		this.remove_current_child = function() {
 			if (this.child === undefined) return;
 			this.el.removeChild(this.el.children[this.el.children.length - 1]);
 			this.child = undefined;
+		}.bind(this);
+
+		this.node = node;
+		this.parent = parent;
+		this.child = undefined;
+		this.el = build("div", "taxonomy_item");
+		this.callback_changed = callback_changed || function(value) {};
+		this._value = undefined;
+
+
+		Object.defineProperty(this, "value", {
+			"get": function() { return this.root._value; }.bind(this),
+			"set": function(v) {
+				this.root._value = v;
+				if (!(v in this.paths)) return;
+				var path = this.paths[v];
+				var ti = this;
+
+				for (var i = 1; i < path.length; i++) {
+					ti.select(ti.get_node_by_tid(path[i]));
+					ti = ti.child;
+				}
+			}.bind(this)
+		});
+
+		if (parent === undefined) {
+			this.el_del = build("div", "edf_mini_button", this.el, "❌");
+			this.el_del.addEventListener("click", function() {
+				this.callback_changed(undefined, this.value);
+				this.el.parentElement.removeChild(this.el);
+			}.bind(this));
+
+			this.el.setAttribute("data-root", "");
+			this.root = this;
+		} else {
+			parent.el.appendChild(this.el);
+			this.root = parent.root;
 		}
+
+		this.el_sel = build("select", undefined, this.el);
+		build("option", undefined, this.el_sel, "-");
+		for (var i = 0; i < node.children.length; i++) {
+			var opt = build("option", undefined, this.el_sel, node.children[i].name);
+			opt.value = node.children[i].tid;
+		}
+
+		this.el_sel.addEventListener("change", function() { this.select(); }.bind(this));
+
+		this.paths = {};
+		this.map_node_path(node, []);
 	}
 
 
-	class node_list_editor {
 
-		constructor(el) {
-			this.orig = el.querySelector("input");
-			this.orig.type = "hidden";
+	function node_list_editor(el) {
 
-			this.output = build("input", undefined, el);
-			this.output.type = "hidden";
-			this.output.name = "learning_outcomes_json";
-			this.output.value = "[]";
-
-			this.structure = JSON.parse(el.getAttribute("data-node_list_editor"));
-			this.el_fields = [];
-
-			this.el = build("div", "node_list_editor", el);
-			this.el.setAttribute("data-empty", "");
-			this.el_tabber = build("div", "tabber", this.el);
-			this.el_form = build("div", "form", this.el);
-
-			this.items = [];
-			this._current_item = undefined;
-			this.form = [];
-
-			for (var i = 0; i < this.structure.length; i++) {
-				var field = this.structure[i];
-				switch(field.type) {
-					case "text_area":
-						this.add_text_area(field);
-						break;
-					case "taxonomy_list":
-						this.add_taxonomy_list(field);
-						break;
-					case "name":
-						this.add_name(field);
-						break;
+		Object.defineProperty(this, "current_item", {
+			"get": function() { return this._current_item; },
+			"set": function(item) {
+				//	Tab
+				if (this._current_item !== undefined) {
+					this._current_item.el.removeAttribute("data-active");
 				}
-			}
 
-			this.el_add_tab = build("div", "add tab", this.el_tabber, "Add");
-			this.el_add_tab.addEventListener("click", function() {	
-				this.current_item = this.add_item();
-			}.bind(this));
+				this._current_item = item;
 
-			this.el_del = build("div", "button delete", this.el_form, "Delete");
-			this.el_del.addEventListener("click", function() {
-				this.rem_item(this.current_item);
-				if (this.items.length > 0) this.current_item = this.items[0];
-				else this.current_item = undefined;
-			}.bind(this));
+				if (item === undefined) {
+					this.el.setAttribute("data-empty", "");
+				} else {
+					this.el.removeAttribute("data-empty");
+					item.el.setAttribute("data-active", "");
 
-			var data = JSON.parse(el.getAttribute("data-node_list_data"));
-			for (var i = 0; i < data.length; i++) {
-				this.add_item(data[i]);
-			}
-			if (this.items.length > 0) this.current_item = this.items[0];
-
-		}
-
-		get current_item() { return this._current_item; }
-		set current_item(item) {
-			//	Tab
-			if (this._current_item !== undefined) {
-				this._current_item.el.removeAttribute("data-active");
-			}
-
-			this._current_item = item;
-
-			if (item === undefined) {
-				this.el.setAttribute("data-empty", "");
-			} else {
-				this.el.removeAttribute("data-empty");
-				item.el.setAttribute("data-active", "");
-
-				//	Form
-				for (var i = 0; i < this.structure.length; i++) {
-					var field = this.structure[i];
-					switch(field.type) {
-						case "text_area":
-							field.el.value = item[field.name];
-							break;
-						case "taxonomy_list":
-							field.el.innerHTML = "";
-							for (var j = 0; j < item[field.name].length; j++) {
-								var tx = this.add_taxonomy_item(field);
-								tx.value = item[field.name][j];
-							}
-							break;
-						case "name":
-							field.el.value = item[field.name];
-							break;
+					//	Form
+					for (var i = 0; i < this.structure.length; i++) {
+						var field = this.structure[i];
+						switch(field.type) {
+							case "text_area":
+								field.el.value = item[field.name];
+								break;
+							case "taxonomy_list":
+								field.el.innerHTML = "";
+								for (var j = 0; j < item[field.name].length; j++) {
+									var tx = this.add_taxonomy_item(field);
+									tx.value = item[field.name][j];
+								}
+								break;
+							case "name":
+								field.el.value = item[field.name];
+								break;
+						}
 					}
 				}
 			}
+		});
 
-		}
-
-		create_value() {
+		this.create_value = function() {
 			var items = [];
 			for (var i = 0; i < this.items.length; i++) {
 				var r_item = this.items[i];
@@ -226,9 +170,9 @@
 				items.push(item);
 			}
 			this.output.value = JSON.stringify(items);
-		}
+		}.bind(this);
 
-		increment_name(name) {
+		this.increment_name = function(name) {
 			var biggest = 0;
 			for (var i = 0; i < this.items.length; i++) {
 				var item_name = this.items[i].name;
@@ -241,14 +185,14 @@
 			}
 			
 			return name + " " + (biggest + 1);
-		}
+		}.bind(this);
 
-		rem_item(item) {
+		this.rem_item = function(item) {
 			this.items.splice(this.items.indexOf(item), 1);
 			this.el_tabber.removeChild(item.el);
-		}
+		}.bind(this);
 
-		add_item(data) {
+		this.add_item = function(data) {
 			var item = {};
 			for (var i = 0; i < this.structure.length; i++) {
 				var field = this.structure[i];
@@ -279,9 +223,9 @@
 
 			item["el"] = el_tab;
 			return item;
-		}
+		}.bind(this);
 
-		add_text_area(field) {
+		this.add_text_area = function(field) {
 			var el = build("div", "text_area", this.el_form);
 			var el_title = build("div", "title", el, field.title);
 			var el_area = build("textarea", undefined, el);
@@ -294,9 +238,9 @@
 			}.bind(this);
 			el_area.addEventListener("keyup", changefunc);
 			el_area.addEventListener("mouseup", changefunc);
-		}
+		}.bind(this);
 
-		add_taxonomy_item(field) {
+		this.add_taxonomy_item = function(field) {
 			var tax_changed = function(val, old_val) {
 				var idx = this.current_item[field.name].indexOf(old_val);
 				if (idx != -1) {
@@ -311,9 +255,9 @@
 			var tx = new taxonomy_item(field.taxonomy_tree, undefined, tax_changed);
 			field.el.appendChild(tx.el);
 			return tx;
-		}
+		}.bind(this);
 
-		add_taxonomy_list(field) {
+		this.add_taxonomy_list = function(field) {
 			var el = build("div", "taxonomy_list", this.el_form);
 			var el_title = build("div", "title", el, field.title);
 			var el_taxonomies = build("div", "taxonomies", el);
@@ -321,9 +265,9 @@
 			field.el = el_taxonomies;
 
 			el_add_button.addEventListener("click", function() { this.add_taxonomy_item(field); }.bind(this) );
-		}
+		}.bind(this);
 
-		add_name(field) {
+		this.add_name = function(field) {
 			var el = build("div", "text", this.el_form);
 			var el_title = build("div", "title", el, field.title);
 			var el_inp = build("input", undefined, el);
@@ -337,8 +281,61 @@
 			}.bind(this);
 			el_inp.addEventListener("keyup", changefunc);
 			el_inp.addEventListener("mouseup", changefunc);
+		}.bind(this);
+
+
+		this.orig = el.querySelector("input");
+		this.orig.type = "hidden";
+
+		this.output = build("input", undefined, el);
+		this.output.type = "hidden";
+		this.output.name = "learning_outcomes_json";
+		this.output.value = "[]";
+
+		this.structure = JSON.parse(el.getAttribute("data-node_list_editor"));
+		this.el_fields = [];
+
+		this.el = build("div", "node_list_editor", el);
+		this.el.setAttribute("data-empty", "");
+		this.el_tabber = build("div", "tabber", this.el);
+		this.el_form = build("div", "form", this.el);
+
+		this.items = [];
+		this._current_item = undefined;
+		this.form = [];
+
+		for (var i = 0; i < this.structure.length; i++) {
+			var field = this.structure[i];
+			switch(field.type) {
+				case "text_area":
+					this.add_text_area(field);
+					break;
+				case "taxonomy_list":
+					this.add_taxonomy_list(field);
+					break;
+				case "name":
+					this.add_name(field);
+					break;
+			}
 		}
 
+		this.el_add_tab = build("div", "add tab", this.el_tabber, "Add");
+		this.el_add_tab.addEventListener("click", function() {	
+			this.current_item = this.add_item();
+		}.bind(this));
+
+		this.el_del = build("div", "button delete", this.el_form, "Delete");
+		this.el_del.addEventListener("click", function() {
+			this.rem_item(this.current_item);
+			if (this.items.length > 0) this.current_item = this.items[0];
+			else this.current_item = undefined;
+		}.bind(this));
+
+		var data = JSON.parse(el.getAttribute("data-node_list_data"));
+		for (var i = 0; i < data.length; i++) {
+			this.add_item(data[i]);
+		}
+		if (this.items.length > 0) this.current_item = this.items[0];
 	}
 
 
