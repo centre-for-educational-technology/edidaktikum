6c757fa7d6d2c469ade21dbe26b4d329984110b8
GlebRed <glebredko@gmail.com>
2018-04-06 18:41:31 +0300

    References #516

--- htmlpurifier/ed_iframe.php
+++ htmlpurifier/ed_iframe.php
@@ -43,7 +43,6 @@ class HTMLPurifier_Filter_EdIframe extends HTMLPurifier_Filter
     protected function postFilterCallback($matches)
     {
     	
-    	error_log(print_r($matches, true));
 
         // // Domain Whitelist
         $match = preg_match('#https?://www.youtube(-nocookie)?.com/|player\.vimeo\.com/video/|edpuzzle\.com/embed/|www\.google(?:-nocookie)?\.com/maps/|www\.slideshare(?:-nocookie)?\.net/|docs\.google(?:-nocookie)?\.com/|www\.powtoon(?:-nocookie)?\.com/embed/|onedrive\.live(?:-nocookie)?\.com/|app\.emaze(?:-nocookie)?\.com/|storybird(?:-nocookie)?\.com/books/|e\.issuu\.com/#', $matches[1]);
