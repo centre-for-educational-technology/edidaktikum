c2e0ee70ab7f176bcc2f7eac9e050c749dd5ea44
GlebRed <glebredko@gmail.com>
2018-06-22 14:08:40 +0300

    Closes #605

--- js/ed_likes.js
+++ js/ed_likes.js
@@ -1,21 +1,22 @@
 (function ($) {
-    Drupal.behaviors.ed_likes = {
-        attach: function(context, settings) {
-                    $('.ed-node-statistics', context).find('.ed-node-statistics-likes a').on('click', function(e) {
-                        e.preventDefault();
+Drupal.behaviors.ed_likes = {
+  attach: function(context, settings) {
+    $('.ed-node-statistics', context).find('.ed-node-statistics-likes a').on('click', function(e) {
+      e.preventDefault();
 
-                        $.ajax({
-                            url: Drupal.settings.basePath + Drupal.settings.pathPrefix + 'node/' + $(this).data('id') + '/edidaktikum/action/like',
-                            dataType: 'json',
-                            type: 'POST',
-                            success: function(response) {
-                                if (true == response.success) {
-                                    $(e.currentTarget).parent().find('span.count').html( response.count);
-                                }
-                            }
-                        });
-                    });
-                }
-    };
+      $.ajax({
+        url: Drupal.settings.basePath + Drupal.settings.pathPrefix + 'node/' + $(this).data('id') + '/edidaktikum/action/like',
+        dataType: 'json',
+        type: 'POST',
+        success: function(response) {
+          if (true == response.success) {
+            console.log('erfe');
+            $(e.currentTarget).parent().find('span.ed-likes-count').html( Drupal.t('@count likes', { '@count': response.count }));
+          }
+        }
+      });
+    });
+  }
+};
 })(jQuery);
 
