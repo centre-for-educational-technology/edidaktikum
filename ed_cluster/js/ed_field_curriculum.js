3bb02631bb3b1454c23f2fd942dae199d7ffadb5
Pjotr Savitski <gnum@tlu.ee>
2018-08-30 16:19:47 +0300

    References #624, mostly or almost done.
    Changed curriculum and university fields to get options data from
    functions not internally defined array as it used to be. Implemented a
    small hack to allow igration and purge previously defined options data.
    Existing data would be kept and there should not be any issues with
    identifiers as the identifiers should stay the same. Also implemented
    the JS side logic to make the curriculum field dependent on values
    selected for universities.

---
+++ ed_cluster/js/ed_field_curriculum.js
@@ -0,0 +1,22 @@
+(function($) {
+  Drupal.behaviors.ed_field_curriculum = {
+    'attach': function(context) {
+      $('select[name="ed_field_university[und][]"]', context).on('change', function() {
+        var selected = $(this).val();
+
+        $('select[name="ed_field_curriculum[und][]"]').find('option').each(function() {
+          var ckey = $(this).val();
+          if (ckey === '_none') return;
+
+          var ukey = Drupal.settings.edidaktikum.universities_with_curriculums[ckey];
+          if (ukey !== undefined && $.inArray(ukey.toString(), selected) != -1) {
+            $(this).prop('disabled', false).show();
+          } else {
+            $(this).prop('disabled', true).hide();
+          }
+        });
+      });
+      $('select[name="ed_field_university[und][]"]').trigger('change');
+    }
+  };
+})(jQuery);
