454638209c9d9b9bf6a473a06b7c638716cd4ae0
GlebRed <glebredko@gmail.com>
2019-01-09 16:05:37 +0200

    Closes #688, closes #686

--- ed_cluster/js/ed_task_manager_task_filter.js
+++ ed_cluster/js/ed_task_manager_task_filter.js
@@ -10,6 +10,15 @@
                 }
                 location.href = (Drupal.settings.basePath + path);
             });
+
+
+            //Table with fixed left column used by Task manager
+            $('body.page-node-task-manager .table-responsive').scroll(function(e) {
+
+                $('table.scroll thead').css("left", -$(".table-responsive").scrollLeft());
+                $('table.scroll thead th:nth-child(1)').css("left", $(".table-responsive").scrollLeft());
+                $('table.scroll tbody td:nth-child(1)').css("left", $(".table-responsive").scrollLeft());
+            });
         }
     }
 })(jQuery);
