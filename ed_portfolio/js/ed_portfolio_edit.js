a0dada379a65b29a6277b726d895a68a68d7f5fd
GlebRed <glebredko@gmail.com>
2019-12-17 13:47:06 +0200

    References #791

--- ed_portfolio/js/ed_portfolio_edit.js
+++ ed_portfolio/js/ed_portfolio_edit.js
@@ -1,131 +1,44 @@
 (function ($) {
   Drupal.behaviors.ed_portfolio = {
-    attach: function(context, settings) {
-      function add_spinner(modal) {
-        $(modal).find('.modal-body').append('<i class="fa fa-spinner fa-spin" aria-hidden="true"></i>');
-      }
+    attach: function (context, settings) {
 
-      function remove_spinner(modal) {
-        $(modal).find('.modal-body').find('i.fa.fa-spinner.fa-spin').remove();
-      }
+      var keys = [];
 
-      // Add modal part
-      var addModalElement = $('#add-to-collection-modal'),
-      addNid = addModalElement.next('a').data('nid');
+      if ($('.progress').length) {
 
-      addModalElement.find('button.btn-primary').on('click', function() {
-        var collectionsElement = addModalElement.find('select[name="collections"]'),
-        selectedCollections = collectionsElement.val();
 
-        if ( !selectedCollections ) {
-          return false;
+        try {
+          keys = Drupal.settings.ed_portfolio;
+        } catch (e) {
+
         }
 
-        $(this).prop('disabled', true);
-        collectionsElement.prop('disabled', true);
+        for (var key in keys) {
 
-        $.ajax({
-          url: Drupal.settings.basePath + Drupal.settings.pathPrefix + 'collections/user/add/' + addNid,
-          dataType: 'json',
-          type: 'POST',
-          data: { selected_collections : selectedCollections },
-          cache: false,
-          success: function(response) {
-            if (true == response.success) {
-              addModalElement.modal('hide');
+          if (keys.hasOwnProperty(key)) {
+
+            if ($('#progress-' + key).length) {
+
+              var bar = new ProgressBar.Line('#progress-' + key, {
+                strokeWidth: 4,
+                easing: 'easeInOut',
+                duration: 4000,
+                color: '#ff9f00',
+                trailWidth: 4,
+                svgStyle: {width: '100%', height: '100%'},
+                from: {color: '#ff9f00'},
+                to: {color: '#1abc9c'},
+              });
+              bar.animate(keys[key]);  // Value from 0.0 to 1.0
             }
+
+
           }
-        });
-      });
-
-      addModalElement.on('show.bs.modal', function() {
-        var that = this;
-
-        add_spinner(that);
-
-        $.ajax({
-          url: Drupal.settings.basePath + Drupal.settings.pathPrefix + 'collections/user/load/' + addNid,
-          dataType: 'json',
-          type: 'POST',
-          cache: false,
-          success: function(response) {
-            remove_spinner(that);
-
-            if (true == response.success) {
-              if ( response.data ) {
-                $(that).find('button.btn-primary').prop('disabled', false);
-                $(that).find('.modal-body').append('<select name="collections" multiple="multiple" style="width:100%;"></select>');
-                var collectionsElement = $(that).find('select[name="collections"]');
-                $.each(response.data, function(key, single) {
-                  collectionsElement.append('<option value="' + single.nid + '">' + single.title + '</option>');
-                });
-              }
-            }
-          }
-        });
-      }).on('hidden.bs.modal', function() {
-        $(this).find('button.btn-primary').prop('disabled', true);
-        $(this).find('select[name="collections"]').remove();
-      });
-
-      // Remove modal part
-      var removeModalElement = $('#remove-from-collection-modal'),
-      removeNid = removeModalElement.next('a').data('nid');
-
-      removeModalElement.find('button.btn-primary').on('click', function() {
-        var collectionsElement = removeModalElement.find('select[name="collections"]'),
-        selectedCollections = collectionsElement.val();
-
-        if ( !selectedCollections ) {
-          return false;
         }
 
-        $(this).prop('disabled', true);
-        collectionsElement.prop('disabled', true);
 
-        $.ajax({
-          url: Drupal.settings.basePath + Drupal.settings.pathPrefix + 'collections/learning_resource/remove/' + removeNid,
-          dataType: 'json',
-          type: 'POST',
-          data: { selected_collections : selectedCollections },
-          cache: false,
-          success: function(response) {
-            if (true == response.success) {
-              removeModalElement.modal('hide');
-            }
-          }
-        });
-      });
+      }
 
-      removeModalElement.on('show.bs.modal', function() {
-        var that = this;
-
-        add_spinner(that);
-
-        $.ajax({
-          url: Drupal.settings.basePath + Drupal.settings.pathPrefix + 'collections/learning_resource/load/' + removeNid,
-          dataType: 'json',
-          type: 'POST',
-          cache: false,
-          success: function(response) {
-            remove_spinner(that);
-
-            if (true == response.success) {
-              if ( response.data ) {
-                $(that).find('button.btn-primary').prop('disabled', false);
-                $(that).find('.modal-body').append('<select name="collections" multiple="multiple" style="width:100%;"></select>');
-                var collectionsElement = $(that).find('select[name="collections"]');
-                $.each(response.data, function(key, single) {
-                  collectionsElement.append('<option value="' + single.nid + '">' + single.title + '</option>');
-                });
-              }
-            }
-          }
-        });
-      }).on('hidden.bs.modal', function() {
-        $(this).find('button.btn-primary').prop('disabled', true);
-        $(this).find('select[name="collections"]').remove();
-      });
     }
   };
 })(jQuery);
